63d3216385f3ca1ea77899e9712efc13f5a52b9e
cmitchell7376 <cmitchell7376@yahoo.com>
2019-06-15 09:11:42 -0500

    initial commit

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,14 +1,15 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 /**
  * Created by LaunchCode
@@ -20,10 +21,11 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index(Model model, int id) {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String index(Model model,@PathVariable int id) {
 
         // TODO #1 - get the Job with the given ID and pass it into the view
+        model.addAttribute("job",JobData.getInstance().findById(id));
 
         return "job-detail";
     }
@@ -35,13 +37,19 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @Valid JobForm jobForm, Errors errors, @ModelAttribute Job job, @RequestParam int employerId) {
 
         // TODO #6 - Validate the JobForm model, and if valid, create a
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
+        if(errors.hasErrors()){
+            model.addAttribute(new JobForm());
+            return "new-job";
+        }
+        job.setEmployer(JobData.getInstance().getEmployers().findById(employerId));
+        JobData.getInstance().add(job);
 
-        return "";
+        return "redirect:" + job.getId();
 
     }
 }
--- src/main/java/org/launchcode/models/forms/JobForm.java
+++ src/main/java/org/launchcode/models/forms/JobForm.java
@@ -18,6 +18,9 @@ public class JobForm {
     @NotNull
     @Size(min=1, message = "Name may not be empty")
     private String name;
+    private Location location;
+    private CoreCompetency coreCompetency;
+    private PositionType positionType;
 
     @NotNull
     private int employerId;
@@ -40,55 +43,68 @@ public class JobForm {
         /*
             TODO #4 - populate the other ArrayList collections needed in the view
         */
-
         employers = jobData.getEmployers().findAll();
+        locations = jobData.getLocations().findAll();
+        coreCompetencies = jobData.getCoreCompetencies().findAll();
+        positionTypes = jobData.getPositionTypes().findAll();
 
     }
 
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public int getEmployerId() {
         return employerId;
     }
-
-    public void setEmployerId(int employerId) {
-        this.employerId = employerId;
+    public Location getLocation() {
+        return location;
+    }
+    public CoreCompetency getCoreCompetency() {
+        return coreCompetency;
+    }
+    public PositionType getPositionType() {
+        return positionType;
     }
 
     public ArrayList<Employer> getEmployers() {
         return employers;
     }
-
-    public void setEmployers(ArrayList<Employer> employers) {
-        this.employers = employers;
-    }
-
     public ArrayList<Location> getLocations() {
         return locations;
     }
-
-    public void setLocations(ArrayList<Location> locations) {
-        this.locations = locations;
-    }
-
     public ArrayList<CoreCompetency> getCoreCompetencies() {
         return coreCompetencies;
     }
-
-    public void setCoreCompetencies(ArrayList<CoreCompetency> coreCompetencies) {
-        this.coreCompetencies = coreCompetencies;
-    }
-
     public ArrayList<PositionType> getPositionTypes() {
         return positionTypes;
     }
 
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmployerId(int employerId) {
+        this.employerId = employerId;
+    }
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+    public void setCoreCompetency(CoreCompetency coreCompetence) {
+        this.coreCompetency = coreCompetence;
+    }
+    public void setPositionType(PositionType positionType) {
+        this.positionType = positionType;
+    }
+
+    public void setEmployers(ArrayList<Employer> employers) {
+        this.employers = employers;
+    }
+    public void setLocations(ArrayList<Location> locations) {
+        this.locations = locations;
+    }
+    public void setCoreCompetencies(ArrayList<CoreCompetency> coreCompetencies) {
+        this.coreCompetencies = coreCompetencies;
+    }
     public void setPositionTypes(ArrayList<PositionType> positionTypes) {
         this.positionTypes = positionTypes;
     }
