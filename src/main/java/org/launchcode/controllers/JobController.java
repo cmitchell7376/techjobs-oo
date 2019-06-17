d43953958fc7d88b4e14e3721e7c8f92d852357a
cmitchell7376 <cmitchell7376@yahoo.com>
2019-06-17 17:45:36 -0500

    error fixed

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -43,7 +43,6 @@ public class JobController {
         // new Job and add it to the jobData data store. Then
         // redirect to the job detail view for the new Job.
         if(errors.hasErrors()){
-            model.addAttribute(new JobForm());
             return "new-job";
         }
         job.setEmployer(JobData.getInstance().getEmployers().findById(employerId));
